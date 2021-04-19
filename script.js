2effc8e8705dcbcd961944443f40e3e950600583
lorettekraaijenbrink <64962971+lorettekraaijenbrink@users.noreply.github.com>
2021-04-19 17:33:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const age = 18;
-const isFemale = false
-const driverStatus = 'boob'
+const isFemale = false;
+const driverStatus = 'bob';
 
 if (age >= 18) {
-    console.log("Je bent oud genoeg om naar binnen te mogen")
+    console.log("Je bent oud genoeg om naar binnen te mogen");
 } else {
-    console.log("Je bent te jong en moet buiten blijven")
+    console.log("Je bent te jong en moet buiten blijven");
 }
 
 if (isFemale) {
-    console.log("Je bent een vrouw")
+    console.log("Je bent een vrouw");
 } else {
-    console.log("Helaas, het is ladiesnight, dus vanavond ben je niet welkom")
+    console.log("Helaas, het is ladiesnight, dus vanavond ben je niet welkom");
 }
 
 if (driverStatus == "bob") {
-    console.log("Fijn bob, jij mag rijden")
+    console.log("Fijn bob, jij mag rijden");
 } else {
-    console.log("Geen bob, dus niet achter het stuur kruipen!")
+    console.log("Geen bob, dus niet achter het stuur kruipen!");
 }
